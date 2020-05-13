19f0c9f3441f0cbcdd1c7d57d089c83a7324dc98
johanaluna <johana.luna87@gmail.com>
2020-05-13 20:39:00 +0100

    code interviews

---
+++ CodeSignal/areEquallyStrong.py
@@ -0,0 +1,22 @@
+"""
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are 
+equally strong (the strongest arm can be both the right and the left), 
+and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+"""
+
+def areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight):
+    # conditions 
+    c1 = (yourLeft == friendsLeft) and (yourRight == friendsRight)
+    c2 = (yourLeft == friendsRight) and (yourRight ==friendsLeft)
+    return(True if c1 or c2 else False)
+
+yourLeft = 10
+yourRight = 15
+friendsLeft = 15
+friendsRight = 10
+print(areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight))
+
