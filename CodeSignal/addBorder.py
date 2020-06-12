54ed629c483ebaa87a59a4d30af1298bec28c8fc
johanaluna <johana.luna87@gmail.com>
2020-06-12 18:23:00 +0100

    code interviews

---
+++ CodeSignal/addBorder.py
@@ -0,0 +1,16 @@
+def addBorder(picture):
+    temp = "*"*(len(picture[0]))
+    
+    n = len(picture)
+    
+    for i in range(n):
+        pivot = picture[i]
+        picture[i] = "*" + temp + "*"
+        temp = pivot
+    picture.append("*" + temp + "*")
+    picture.append(picture[0])
+
+    return picture
+
+picture = ["abcde","fghij"]
+print(addBorder(picture))
